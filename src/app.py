b68d0d9c9725eabc31e848e6fcf14ddf76124f32
Anurag Garg <garga6001@gmail.com>
2026-07-16 18:22:10 +0530

    feat: add expense collection endpoint

--- src/app.py
+++ src/app.py
@@ -19,42 +19,48 @@ def about():
         "learning": "Flask Backend Development"
     })
 
+
 @app.route("/contact")
 def contact():
     return "Contact: backend-team@expenseflow.local"
 
+
+expenses = {
+    1: {
+        "expense_id": 1,
+        "amount": 500,
+        "category": "Food",
+        "description": "Pizza with friends",
+        "payment_method": "UPI",
+        "expense_type": "Personal",
+        "priority": "Need",
+        "status": "Paid"
+    },
+    2: {
+        "expense_id": 2,
+        "amount": 1500,
+        "category": "Shopping",
+        "description": "Shopping with brother",
+        "payment_method": "UPI",
+        "expense_type": "Personal",
+        "priority": "Want",
+        "status": "Paid"
+    },
+    3: {
+        "expense_id": 3,
+        "amount": 700,
+        "category": "Travel",
+        "description": "Travel from Bhopal to Indore",
+        "payment_method": "Cash",
+        "expense_type": "Personal",
+        "priority": "Need",
+        "status": "Paid"
+    }
+}
+
+
 @app.route("/expense/<int:expense_id>")
 def expense(expense_id):
-
-    expenses = {
-        1: {
-            "amount": 500,
-            "category": "Food",
-            "description": "Pizza with friends",
-            "payment_method": "UPI",
-            "expense_type": "Personal",
-            "priority": "Need",
-            "status": "Paid"
-        },
-        2: {
-            "amount": 1500,
-            "category": "Shopping",
-            "description": "Shopping with brother",
-            "payment_method": "UPI",
-            "expense_type": "Personal",
-            "priority": "Want",
-            "status": "Paid"
-        },
-        3: {
-            "amount": 700,
-            "category": "Travel",
-            "description": "Travel from Bhopal to Indore",
-            "payment_method": "Cash",
-            "expense_type": "Personal",
-            "priority": "Need",
-            "status": "Paid"
-        }
-    }
 
     expense_data = expenses.get(expense_id)
 
@@ -62,6 +68,13 @@ def expense(expense_id):
         return jsonify({"error": "Expense not found"}), 404
 
     return jsonify(expense_data)
+
+
+@app.route("/expenses")
+def all_expenses():
+
+    return jsonify(list(expenses.values()))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
